6292a263828ff40338475338459dafe1d9c6a890
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-10-11 22:20:02 +0200

    Update RuleName.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,11 +1,105 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import java.sql.Timestamp;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "rulename")
+@Table(name = "RuleName")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id")
+	private Integer id;
+
+	@Column(name = "name", length = 125)
+	private String name;
+
+	@Column(name = "description", length = 125)
+	private String description;
+
+	@Column(name = "json", length = 125)
+	private String json;
+
+	@Column(name = "template", length = 512)
+	private String template;
+
+	@Column(name = "sqlStr", length = 125)
+	private String sqlStr;
+
+	@Column(name = "sqlPart", length = 125)
+	private String sqlPart;
+
+	public RuleName() {
+
+	}
+
+	public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
+		this.name = name;
+		this.description = description;
+		this.json = json;
+		this.template = template;
+		this.sqlStr = sqlStr;
+		this.sqlPart = sqlPart;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	public String getSqlStr() {
+		return sqlStr;
+	}
+
+	public void setSqlStr(String sqlStr) {
+		this.sqlStr = sqlStr;
+	}
+
+	public String getSqlPart() {
+		return sqlPart;
+	}
+
+	public void setSqlPart(String sqlPart) {
+		this.sqlPart = sqlPart;
+	}
 }
