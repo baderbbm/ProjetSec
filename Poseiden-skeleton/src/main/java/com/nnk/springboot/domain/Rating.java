375884d5ac3f00d73a8bed83d1599441f0e123cd
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-10-11 22:19:29 +0200

    Update Rating.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,83 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import java.sql.Timestamp;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "rating")
+@Table(name = "Rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+
+	
+	   public Rating() {
+			
+		}
+	
+    public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
+		this.moodysRating = moodysRating;
+		this.sandPRating = sandPRating;
+		this.fitchRating = fitchRating;
+		this.orderNumber = orderNumber;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer id;
+
+    @Column(name = "moodysRating", length = 125)
+    private String moodysRating;
+
+    @Column(name = "sandPRating", length = 125)
+    private String sandPRating;
+
+    @Column(name = "fitchRating", length = 125)
+    private String fitchRating;
+
+    @Column(name = "orderNumber")
+    private Integer orderNumber;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMoodysRating() {
+		return moodysRating;
+	}
+
+	public void setMoodysRating(String moodysRating) {
+		this.moodysRating = moodysRating;
+	}
+
+	public String getSandPRating() {
+		return sandPRating;
+	}
+
+	public void setSandPRating(String sandPRating) {
+		this.sandPRating = sandPRating;
+	}
+
+	public String getFitchRating() {
+		return fitchRating;
+	}
+
+	public void setFitchRating(String fitchRating) {
+		this.fitchRating = fitchRating;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
 }
