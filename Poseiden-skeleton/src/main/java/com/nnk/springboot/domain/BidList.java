c63ae8a84b7fb18b7350a5af44a5d409e8543147
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-10-11 22:18:20 +0200

    Update BidList.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,309 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
-
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
-import java.sql.Timestamp;
+import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "bidlist")
+@Table(name = "BidList")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BidListId")
+    private int id;
+
+    @Column(name = "account", nullable = false, length = 30)
+    private String account;
+
+    @Column(name = "type", nullable = false, length = 30)
+    private String type;
+
+    @Column(name = "bidQuantity")
+    private Double bidQuantity;
+
+    @Column(name = "askQuantity")
+    private Double askQuantity;
+
+    @Column(name = "bid")
+    private Double bid;
+
+    @Column(name = "ask")
+    private Double ask;
+
+    @Column(name = "benchmark", length = 125)
+    private String benchmark;
+
+    @Column(name = "bidListDate")
+    private Date bidListDate;
+
+    @Column(name = "commentary", length = 125)
+    private String commentary;
+
+    @Column(name = "security", length = 125)
+    private String security;
+
+    @Column(name = "status", length = 10)
+    private String status;
+
+    @Column(name = "trader", length = 125)
+    private String trader;
+
+    @Column(name = "book", length = 125)
+    private String book;
+
+    @Column(name = "creationName", length = 125)
+    private String creationName;
+
+    @Column(name = "creationDate")
+    private Date creationDate;
+
+    @Column(name = "revisionName", length = 125)
+    private String revisionName;
+
+    @Column(name = "revisionDate")
+    private Date revisionDate;
+
+    @Column(name = "dealName", length = 125)
+    private String dealName;
+
+    @Column(name = "dealType", length = 125)
+    private String dealType;
+
+    @Column(name = "sourceListId", length = 125)
+    private String sourceListId;
+
+    @Column(name = "side", length = 125)
+    private String side;
+
+    
+	public BidList(String account, String type, Double bidQuantity) {
+		this.account = account;
+		this.type = type;
+		this.bidQuantity = bidQuantity;
+	}
+
+
+	public BidList() {
+    }
+  
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getAccount() {
+		return account;
+	}
+
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public Double getBidQuantity() {
+		return bidQuantity;
+	}
+
+
+	public void setBidQuantity(Double bidQuantity) {
+		this.bidQuantity = bidQuantity;
+	}
+
+
+	public Double getAskQuantity() {
+		return askQuantity;
+	}
+
+
+	public void setAskQuantity(Double askQuantity) {
+		this.askQuantity = askQuantity;
+	}
+
+
+	public Double getBid() {
+		return bid;
+	}
+
+
+	public void setBid(Double bid) {
+		this.bid = bid;
+	}
+
+
+	public Double getAsk() {
+		return ask;
+	}
+
+
+	public void setAsk(Double ask) {
+		this.ask = ask;
+	}
+
+
+	public String getBenchmark() {
+		return benchmark;
+	}
+
+
+	public void setBenchmark(String benchmark) {
+		this.benchmark = benchmark;
+	}
+
+
+	public Date getBidListDate() {
+		return bidListDate;
+	}
+
+
+	public void setBidListDate(Date bidListDate) {
+		this.bidListDate = bidListDate;
+	}
+
+
+	public String getCommentary() {
+		return commentary;
+	}
+
+
+	public void setCommentary(String commentary) {
+		this.commentary = commentary;
+	}
+
+
+	public String getSecurity() {
+		return security;
+	}
+
+
+	public void setSecurity(String security) {
+		this.security = security;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public String getTrader() {
+		return trader;
+	}
+
+
+	public void setTrader(String trader) {
+		this.trader = trader;
+	}
+
+
+	public String getBook() {
+		return book;
+	}
+
+
+	public void setBook(String book) {
+		this.book = book;
+	}
+
+
+	public String getCreationName() {
+		return creationName;
+	}
+
+
+	public void setCreationName(String creationName) {
+		this.creationName = creationName;
+	}
+
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+
+	public String getRevisionName() {
+		return revisionName;
+	}
+
+
+	public void setRevisionName(String revisionName) {
+		this.revisionName = revisionName;
+	}
+
+
+	public Date getRevisionDate() {
+		return revisionDate;
+	}
+
+
+	public void setRevisionDate(Date revisionDate) {
+		this.revisionDate = revisionDate;
+	}
+
+
+	public String getDealName() {
+		return dealName;
+	}
+
+
+	public void setDealName(String dealName) {
+		this.dealName = dealName;
+	}
+
+
+	public String getDealType() {
+		return dealType;
+	}
+
+
+	public void setDealType(String dealType) {
+		this.dealType = dealType;
+	}
+
+
+	public String getSourceListId() {
+		return sourceListId;
+	}
+
+
+	public void setSourceListId(String sourceListId) {
+		this.sourceListId = sourceListId;
+	}
+
+
+	public String getSide() {
+		return side;
+	}
+
+
+	public void setSide(String side) {
+		this.side = side;
+	}
 }
