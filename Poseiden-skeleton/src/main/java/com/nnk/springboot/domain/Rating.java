003bb74c50babf51d8d2590ba9590bd25060e3a9
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-10-17 22:20:44 +0200

    Update Rating.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -2,44 +2,45 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import java.sql.Timestamp;
-import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Rating")
 public class Rating {
 
-	
-	   public Rating() {
-			
-		}
-	
-    public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id")
+	private Integer id;
+
+	@Column(name = "moodysRating")
+    @NotBlank(message = "Username is mandatory")
+	private String moodysRating;
+
+	@Column(name = "sandPRating")
+    @NotBlank(message = "Username is mandatory")
+	private String sandPRating;
+
+	@Column(name = "fitchRating")
+    @NotBlank(message = "Username is mandatory")
+	private String fitchRating;
+
+	@Column(name = "orderNumber")
+    @Positive(message = "Bid Quantity must be greater than 0")
+	private Integer orderNumber;
+
+	public Rating() {
+
+	}
+
+	public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
 		this.moodysRating = moodysRating;
 		this.sandPRating = sandPRating;
 		this.fitchRating = fitchRating;
 		this.orderNumber = orderNumber;
 	}
-
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
-    private Integer id;
-
-    @Column(name = "moodysRating", length = 125)
-    private String moodysRating;
-
-    @Column(name = "sandPRating", length = 125)
-    private String sandPRating;
-
-    @Column(name = "fitchRating", length = 125)
-    private String fitchRating;
-
-    @Column(name = "orderNumber")
-    private Integer orderNumber;
 
 	public Integer getId() {
 		return id;
