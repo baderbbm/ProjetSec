e8fb7a304389b4a3d0afc4bc6a73c2f1fc2a05e4
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-10-24 22:51:10 +0200

    Update RatingTests.java

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/RatingTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/RatingTests.java
@@ -41,4 +41,23 @@ public class RatingTests {
         Optional<Rating> ratingList = ratingRepository.findById(id);
         assertFalse(ratingList.isPresent());
     }
+    @Test
+    public void ratingInvalidTest() {
+        Rating rating = new Rating("", "", "", -1);
+        if (isValidRating(rating)) {
+            rating = ratingRepository.save(rating);
+            assertNotNull(rating.getId());
+            assertEquals("ValidRating", rating.getMoodysRating());
+        } else {
+            System.out.println("Données non valides : la sauvegarde a échoué");
+        }
+    }
+
+    private boolean isValidRating(Rating rating) {
+        return !rating.getMoodysRating().isEmpty() &&
+               !rating.getSandPRating().isEmpty() &&
+               !rating.getFitchRating().isEmpty() &&
+               rating.getOrderNumber() > 0;
+    }
+
 }
