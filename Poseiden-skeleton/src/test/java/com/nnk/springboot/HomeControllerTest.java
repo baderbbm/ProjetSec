b62441e80c4f97c5f9dbbd65a08e2f173b196a32
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-02 21:19:52 +0100

    Update HomeControllerTest.java

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/HomeControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/HomeControllerTest.java
@@ -1,3 +1,4 @@
+/*
 package com.nnk.springboot;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -33,5 +34,82 @@ public class HomeControllerTest {
     public void testAdminHome() {
     	String viewName = homeController.adminHome(model);
         assertEquals("redirect:/bidList/list", viewName);
+    }
+}
+*/
+package com.nnk.springboot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.ui.Model;
+import com.nnk.springboot.controllers.HomeController;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HomeControllerTest {
+
+    private HomeController homeController;
+
+    @Mock
+    private Model model;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        homeController = new HomeController();
+    }
+
+    @Test
+    public void testHomeAuthenticatedAsAdmin() {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.isAuthenticated()).thenReturn(true);
+        User user = new User("admin", "password",
+            List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        when(authentication.getPrincipal()).thenReturn(user);
+        String viewName = homeController.home(model, authentication);
+        assertEquals("redirect:/admin/home", viewName);
+    }
+
+    @Test
+    public void testHomeAuthenticatedAsUser() {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.isAuthenticated()).thenReturn(true);
+        User user = new User("user", "password",
+            List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        when(authentication.getPrincipal()).thenReturn(user);
+        String viewName = homeController.home(model, authentication);
+        assertEquals("redirect:/user/home", viewName);
+    }
+
+    @Test
+    public void testHomeNotAuthenticated() {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.isAuthenticated()).thenReturn(false);
+        String viewName = homeController.home(model, authentication);
+        assertEquals("redirect:/login", viewName);
+    }
+
+    @Test
+    public void testHomeAuthenticatedWithUnknownRole() {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.isAuthenticated()).thenReturn(true);
+        User user = new User("unknown", "password",
+            List.of(new SimpleGrantedAuthority("ROLE_UNKNOWN")));
+        when(authentication.getPrincipal()).thenReturn(user);
+        String viewName = homeController.home(model, authentication);
+        assertEquals("redirect:/login", viewName);
+    }
+
+    @Test
+    public void testAdminHome() {
+        String viewName = homeController.adminHome(model);
+        assertEquals("redirect:/user/list", viewName);
     }
 }
