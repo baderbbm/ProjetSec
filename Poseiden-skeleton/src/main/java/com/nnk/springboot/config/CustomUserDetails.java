1af4489bfd0fa91584cda82b2bea859a8f2d2961
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-03 18:21:39 +0100

    Update CustomUserDetails.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetails.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetails.java
@@ -8,15 +8,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * CustomUserDetails étend la classe User de Spring Security pour personnaliser le stockage des détails de l'utilisateur.
+ */
 public class CustomUserDetails extends User {
 
+    /**
+     * Constructeur de CustomUserDetails.
+     *
+     * @param user L'objet utilisateur de l'application.
+     */
     public CustomUserDetails(com.nnk.springboot.domain.User user) {
         super(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRole()));
-        System.out.println("username "+ user.getUsername());
-        System.out.println("pass"+ user.getPassword());
-    	System.out.println("role "+ user.getRole());
     }
 
+    /**
+     * Cette méthode convertit les rôles de l'utilisateur en autorités accordées.
+     *
+     * @param roles Les rôles de l'utilisateur, séparés par des virgules.
+     * @return Une collection d'autorités accordées pour l'utilisateur.
+     */
     private static Collection<? extends GrantedAuthority> mapRolesToAuthorities(String roles) {
         List<String> roleList = Arrays.asList(roles.split("\\s*,\\s*")); // Divise les rôles par des virgules
         return roleList.stream()
