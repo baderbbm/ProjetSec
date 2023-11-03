a41786267b1569510a9da253bbf123f4692540ab
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-03 18:34:35 +0100

    Update HomeController.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -7,39 +7,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * Le contrôleur HomeController gère les requêtes liées à la page d'accueil et à
+ * la redirection des utilisateurs.
+ */
 @Controller
 public class HomeController {
-	
-    @RequestMapping("/")
-    public String home(Model model, Authentication authentication) {
-        if (authentication != null && authentication.isAuthenticated()) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            if (userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
-            	System.out.println("Je suis dans ADMIN");
-                return "redirect:/admin/home"; // Rediriger l'administrateur
-            } else if (userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
-            	System.out.println("Je suis dans USER");
-                return "redirect:/user/home"; // Rediriger l'utilisateur
-            }
-        }
-        return "redirect:/login"; // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté ou si son rôle n'est pas USER ou ADMIN 
-    }
-    
+
+	/**
+	 * Gère la requête d'accueil "/" et redirige les utilisateurs en fonction de
+	 * leur rôle.
+	 *
+	 * @param model          Le modèle de données pour la vue.
+	 * @param authentication L'objet d'authentification de l'utilisateur.
+	 * @return Une chaîne représentant la vue vers laquelle rediriger l'utilisateur.
+	 */
+	@RequestMapping("/")
+	public String home(Model model, Authentication authentication) {
+		if (authentication != null && authentication.isAuthenticated()) {
+			UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+			if (userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+				return "redirect:/admin/home";
+			} else if (userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+				return "redirect:/user/home";
+			}
+		}
+		return "redirect:/login";
+	}
+
+	/**
+	 * Gère la requête "/admin/home" et redirige les administrateurs vers la liste
+	 * des utilisateurs.
+	 *
+	 * @param model Le modèle de données pour la vue.
+	 * @return Une chaîne représentant la vue vers laquelle rediriger
+	 *         l'administrateur.
+	 */
 	@RequestMapping("/admin/home")
 	public String adminHome(Model model) {
-		// return "redirect:/bidList/list";
 		return "redirect:/user/list";
 	}
 
-	// ajouter
+	/**
+	 * Gère la requête "/user/home" et redirige les utilisateurs vers la liste des
+	 * offres.
+	 *
+	 * @param model Le modèle de données pour la vue.
+	 * @return Une chaîne représentant la vue vers laquelle rediriger l'utilisateur.
+	 */
 	@RequestMapping("/user/home")
 	public String userHome(Model model) {
 		return "redirect:/bidList/list";
 	}
-	
-	// Affiche la page de connexion
+
+	/**
+	 * Gère la requête GET "/login" et renvoie la vue de la page de connexion.
+	 *
+	 * @return Une chaîne représentant la vue de la page de connexion.
+	 */
 	@GetMapping("/login")
 	public String login() {
 		return "login";
-	}	
+	}
 }
