09b0daf4a1403dc99f9af47f77785ec55ea37a20
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-10 22:28:54 +0100

    Update LoginController.java

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,18 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("login")
+    public LoginController(UserRepository userRepository) {
+		this.userRepository=userRepository;
+	}
+
+	@GetMapping("login")
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
     }
 
-    @GetMapping("secure/article-details")
+   @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userRepository.findAll());
@@ -29,12 +34,16 @@ public class LoginController {
         return mav;
     }
 
-    @GetMapping("error")
-    public ModelAndView error() {
-        ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
-        mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
-        return mav;
-    }
+   @GetMapping("error")
+   public ModelAndView error(Authentication authentication) {
+       ModelAndView mav = new ModelAndView();
+       if (authentication != null && authentication.isAuthenticated()) {
+           String username = authentication.getName();
+           mav.addObject("username", username);
+       }
+       String errorMessage = "You are not authorized for the requested data.";
+       mav.addObject("errorMsg", errorMessage);
+       mav.setViewName("403");
+       return mav;
+   }
 }
