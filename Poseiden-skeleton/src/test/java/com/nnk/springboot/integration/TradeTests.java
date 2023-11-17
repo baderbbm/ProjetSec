030f7cd8e14640612700dee7abd67ec89cc560bb
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-17 19:37:47 +0100

    Add files via upload

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/integration/TradeTests.java
@@ -0,0 +1,56 @@
+package com.nnk.springboot.integration;
+
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.repositories.TradeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TradeTests {
+
+    @Autowired
+    private TradeRepository tradeRepository;
+
+    @Test
+    public void tradeTest() {
+        Trade trade = new Trade("Trade Account", "Type");
+
+        // Save
+        trade = tradeRepository.save(trade);
+        assertNotNull(trade.getTradeId());
+        assertTrue(trade.getAccount().equals("Trade Account"));
+
+        // Update
+        trade.setAccount("Trade Account Update");
+        trade = tradeRepository.save(trade);
+        assertTrue(trade.getAccount().equals("Trade Account Update"));
+
+        // Find
+        List<Trade> listResult = tradeRepository.findAll();
+        assertTrue(listResult.size() > 0);
+
+        // Delete
+        Integer id = trade.getTradeId();
+        tradeRepository.delete(trade);
+        Optional<Trade> tradeList = tradeRepository.findById(id);
+        assertFalse(tradeList.isPresent());
+    }
+    
+    @Test
+    public void tradeInvalidTest() {
+        try {
+            long initialCount = tradeRepository.count();
+            Trade trade = new Trade("", "", -1.0);
+            trade = tradeRepository.save(trade);
+            long finalCount = tradeRepository.count();
+            assertEquals(initialCount, finalCount);
+        } catch (Exception e) {
+        }
+    }
+}
