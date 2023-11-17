a4eb4029ebfe96a1431753a25ebc48342396411e
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-11-17 19:38:12 +0100

    Add files via upload

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/integration/UserTests.java
@@ -0,0 +1,48 @@
+package com.nnk.springboot.integration;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void userTest() {
+        User user = new User();
+        user.setUsername("testUser");
+        user.setPassword("A3?useruser");
+        user.setFullname("Test User");
+        user.setRole("USER");
+
+        // Save
+        user = userRepository.save(user);
+        assertNotNull(user.getId());
+        assertEquals("testUser", user.getUsername());
+
+        // Update
+        user.setUsername("updatedUser");
+        user = userRepository.save(user);
+        assertEquals("updatedUser", user.getUsername());
+
+        // Find
+        List<User> listResult = userRepository.findAll();
+        assertTrue(listResult.size() > 0);
+
+        // Delete
+        Integer id = user.getId();
+        userRepository.delete(user);
+        Optional<User> userList = userRepository.findById(id);
+        assertFalse(userList.isPresent());
+    }
+}
